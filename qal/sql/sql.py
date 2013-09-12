122b7ec35493ebe33ba4a910fd59412ab3ef45f9
Nicklas <nibo@nibo01.(none)>
2013-09-12 10:41:38 +0200

    Move all super classes out of dal.sql.sql

--- qal/sql/sql.py
+++ qal/sql/sql.py
@@ -11,99 +11,23 @@ For example: sources means that it is a list of Parameter_Source.
 @warning: Changes and new classes must satisfy both the import/export of data structures and schema generation.
 
 """
-from qal.sql.sql_types import DEFAULT_ROWSEP, expression_item_types,tabular_expression_item_types
-from qal.dal.dal_types import DB_POSTGRESQL, DB_MYSQL, DB_ORACLE, DB_DB2, DB_SQLSERVER 
 
-from qal.sql.sql_utils import *
+from qal.sql.sql_types import DEFAULT_ROWSEP, expression_item_types,tabular_expression_item_types
+from qal.dal.dal_types import DB_POSTGRESQL, DB_MYSQL, DB_ORACLE, DB_DB2 # , DB_SQLSERVER 
+
+from qal.sql.sql_base import Parameter_Base, Parameter_Remotable, SQL_List, Parameter_Expression_Item
+
+
+from qal.sql.sql_utils import add_operator, parenthesise, oracle_add_escape, add_comma, make_operator, check_for_param_content,\
+                                none_as_sql, error_on_blank, comma_separate, make_function, db_specific_operator, db_specific_object_reference,\
+                                 citate,check_not_null, curr_user, db_specific_datatype, curr_datetime, add_comma_rs, oracle_create_auto_increment
 from qal.sql.sql_types import condition_part
 from qal.nosql.flatfile import Flatfile_Dataset
 from qal.nosql.rdbms import RDBMS_Dataset
 from qal.nosql.xml import XML_Dataset
 from qal.nosql.matrix import Matrix_Dataset
-                              
-class Parameter_Base(object): 
-    """This class is a base class for all parameter classes."""
-    _row_separator = DEFAULT_ROWSEP 
 
-    def __init__(self, _row_separator = None):
-        super(Parameter_Base, self ).__init__()
-        if _row_separator != None: 
-            self._row_separator = _row_separator
 
-    def as_sql(self, _db_type): 
-        """Generate SQL for specified database engine"""
-        raise Exception(self.__class__.__name__ + ".as_sql() is not implemented")
- 
-
-class Parameter_Remotable(object): 
-    """This class is a base class for all classes that is remotable. 
-    That is, they can fetch their data from, or perform their actions at, a different location than the parent class.
-    If they return data, the data will be held in the temporary table, where it can be joined with or otherwise managed.
-    """
-    """The temporary table name is automatically generated based on the temporary table_name prefix."""
-    temporary_table_name = None
-    """The temporary table name is automatically generated based on the temporary table_name prefix.
-    Its default is "t_". """
-    temporary_table_name_prefix = "t_"   
-    connection_guid = None
-    
-    def __init__(self,  _connection_guid=None, _temporary_table_name_prefix = None ):
-        super(Parameter_Remotable, self ).__init__()
-        if _connection_guid != None: 
-            self.connection_guid = _connection_guid
-
-        if _temporary_table_name_prefix != None: 
-            self._temporary_table_name_prefix = _temporary_table_name_prefix
-
-    def as_sql(self, _db_type): 
-        """Make connection to resource defined the connection_guid"""
-        pass
-        """Run query"""
-        pass
-        """Generate table name"""
-        pass
-        """Create temporary table using column names and types from result set"""
-        pass
-        """Insert data into temporary table""" 
-        
-        raise Exception(self.__class__.__name__ + ".as_sql() is not implemented")
- 
- 
-
- 
-class SQL_List(list):
-    """This is the base class for lists of class instances."""    
-
-    def __init__(self, _itemclasses = None):
-        super(SQL_List, self ).__init__()
-        if _itemclasses != None:
-            self._itemclasses = _itemclasses
-
-    def as_sql(self, _db_type): 
-        """Generate SQL for specified database engine"""
-        result = '' 
-        for _item in self:
-#            TODO: Check if the below list functionality is ever needed/wanted.             
-#            result+= DEFAULT_ROWSEP + '--****************' + str(self.itemclasses) + ' number ' + index +'****************************' + DEFAULT_ROWSEP 
-            if hasattr(_item, 'as_sql'):
-                result+= _item.as_sql(_db_type)
-            else:
-                result+= _item
-
-        return result
-
-   
-class Parameter_Expression_Item(Parameter_Base):
-    """The superclass of all classes that can be considered part of an expression"""
-    operator = 'C'  
-    
-    def __init__(self,_operator = None):
-        super(Parameter_Expression_Item, self ).__init__()
-        if _operator != None:
-            self.operator = _operator
-        else:
-            self.operator = 'C' 
-    
 
 class Parameter_Expression(Parameter_Expression_Item):
     """Holds an expression"""
---
+++ qal/sql/sql_base.py
@@ -0,0 +1,97 @@
+"""
+Created on Sep 12, 2013
+
+@author: Nicklas Boerjesson
+"""
+
+""" 
+This module contains all the base classes for the SQL-related code.
+The reason is simply to make things more manageable and split things up a bit.
+"""
+from qal.sql.sql_types import DEFAULT_ROWSEP
+
+class Parameter_Base(object): 
+    """This class is a base class for all parameter classes."""
+    _row_separator = DEFAULT_ROWSEP 
+
+    def __init__(self, _row_separator = None):
+        super(Parameter_Base, self ).__init__()
+        if _row_separator != None: 
+            self._row_separator = _row_separator
+
+    def as_sql(self, _db_type): 
+        """Generate SQL for specified database engine"""
+        raise Exception(self.__class__.__name__ + ".as_sql() is not implemented")
+ 
+
+class Parameter_Remotable(object): 
+    """This class is a base class for all classes that is remotable. 
+    That is, they can fetch their data from, or perform their actions at, a different location than the parent class.
+    If they return data, the data will be held in the temporary table, where it can be joined with or otherwise managed.
+    """
+    """The temporary table name is automatically generated based on the temporary table_name prefix."""
+    temporary_table_name = None
+    """The temporary table name is automatically generated based on the temporary table_name prefix.
+    Its default is "t_". """
+    temporary_table_name_prefix = "t_"   
+    connection_guid = None
+    
+    def __init__(self,  _connection_guid=None, _temporary_table_name_prefix = None ):
+        super(Parameter_Remotable, self ).__init__()
+        if _connection_guid != None: 
+            self.connection_guid = _connection_guid
+
+        if _temporary_table_name_prefix != None: 
+            self._temporary_table_name_prefix = _temporary_table_name_prefix
+
+    def as_sql(self, _db_type): 
+        """Make connection to resource defined the connection_guid"""
+        pass
+        """Run query"""
+        pass
+        """Generate table name"""
+        pass
+        """Create temporary table using column names and types from result set"""
+        pass
+        """Insert data into temporary table""" 
+        
+        raise Exception(self.__class__.__name__ + ".as_sql() is not implemented")
+ 
+ 
+
+ 
+class SQL_List(list):
+    """This is the base class for lists of class instances."""    
+
+    def __init__(self, _itemclasses = None):
+        super(SQL_List, self ).__init__()
+        if _itemclasses != None:
+            self._itemclasses = _itemclasses
+
+    def as_sql(self, _db_type): 
+        """Generate SQL for specified database engine"""
+        result = '' 
+        for _item in self:
+#            TODO: Check if the below list functionality is ever needed/wanted.             
+#            result+= DEFAULT_ROWSEP + '--****************' + str(self.itemclasses) + ' number ' + index +'****************************' + DEFAULT_ROWSEP 
+            if hasattr(_item, 'as_sql'):
+                result+= _item.as_sql(_db_type)
+            else:
+                result+= _item
+
+        return result  
+    
+    
+   
+class Parameter_Expression_Item(Parameter_Base):
+    """The superclass of all classes that can be considered part of an expression"""
+    operator = 'C'  
+    
+    def __init__(self,_operator = None):
+        super(Parameter_Expression_Item, self ).__init__()
+        if _operator != None:
+            self.operator = _operator
+        else:
+            self.operator = 'C' 
+    
+
