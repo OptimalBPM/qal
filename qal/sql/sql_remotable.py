0b7df9fb69e67ee8022029a09c24ad097e84c696
Nicklas <nibo@nibo01.(none)>
2013-10-01 00:24:05 +0200

    Continue _bring_into_context-implementation

--- qal/sql/sql_remotable.py
+++ qal/sql/sql_remotable.py
@@ -31,13 +31,23 @@ class Parameter_Remotable(object):
             print(self.__class__.__name__ + "._bring_into_context - Error: _resource not cached")
             raise Exception(self.__class__.__name__ + "._bring_into_context - Error: _resource not cached")
         
+        # TODO: Generate temp table name, max 1 (#) + 7 alphanumeric characters
+        _tmp_table_name = '#tmp'
+        
+        # TODO: Fix so that it is certain that it is the same connection/dal. Can't go by resource. 
         """Make connection to resource defined by the resource_uuid"""
         if self._resource.type == 'rdbms':
             _source_dal = Database_Abstraction_Layer(_resource = self._resource)
             _source_sql = self._generate_sql(_db_type)
             from qal.sql.sql import Parameter_Source, Parameter_Identifier
+            if  isinstance(self, Parameter_Source):
+                print("is Parameter_Source")
+                if isinstance(self.expression[0], Parameter_Identifier):
+                    
+                    _source_sql = 'SELECT * FROM ' + self.expression[0].as_sql(_db_type)
+                    self.expression[0].identifier = _tmp_table_name #_tmp_table_name[1:len(_tmp_table_name)] 
 
-            _data = _source_dal.execute(_source_sql)
+            _data = _source_dal.query(_source_sql)
         elif self._resource.type in ["CUSTOM", "FLATFILE", "MATRIX", "XML"]:
             # Use all NoSQL-datasets
 
@@ -48,24 +58,13 @@ class Parameter_Remotable(object):
                   
         else:
             raise Exception(self.__class__.__name__ + "._bring_into_context - Error: Invalid resource type : " + str(self._resource.type))    
-        
+        print(_source_dal.field_types)
         # This is imported locally, to not interfere with the structure.
         from qal.sql.sql_macros import copy_to_temp_table 
-        _table_name = copy_to_temp_table(_dal = self._dal_ , _values =_data, _field_names = _source_dal.field_names, _field_types = _source_dal.field_types, _table_name = None)
-
+        _parent_dal = Database_Abstraction_Layer(_resource=self._parent_resource)
+        _table_name = copy_to_temp_table(_dal = _parent_dal, _values =_data, _field_names = _source_dal.field_names, _field_types = _source_dal.field_types, _table_name = _tmp_table_name)
+        return _table_name
         
-        
-        
-        print(self.__class__.__name__ + "._bring_into_context: Using "+ self._resource.caption +", resource_uuid: " + str(self.resource_uuid))
-
-        """Generate temporary table name(cannot be more than 8 characters due to some database backend limitations)"""
-
-        pass
-        """Create temporary table using column names and types from result set"""
-        pass
-        """Insert data into temporary table""" 
-        pass
-        return None
 
     def _check_need_prepare(self):
         """
@@ -83,6 +82,7 @@ class Parameter_Remotable(object):
                     return False
                 else:
                     print(self.__class__.__name__ + "._check_need_prepare: Different resource uuid found:" + str(_curr_parent.resource_uuid) + " (own: " +str(self.resource_uuid)+ ")")
+                    self._parent_resource = _curr_parent._resource
                     return True
             _curr_parent = _curr_parent._parent
 
