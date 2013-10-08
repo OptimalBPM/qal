0cf332997f6c5ebf684b40f56cd53cf08bfc24b3
Nicklas <nibo@nibo01.(none)>
2013-10-08 21:29:36 +0200

    Begin development of RPC-fed data sets.

---
+++ qal/nosql/soap.py
@@ -0,0 +1,20 @@
+"""
+Created on Oct 8, 2013
+
+@author: Nicklas Boerjesson
+"""
+
+from qal.nosql.custom import Custom_Dataset
+
+class SOAP_Dataset(Custom_Dataset):
+    """
+    This class implements querying data through SOAP
+    """
+    
+
+    def __init__(self, params):
+        """
+        Constructor
+        """
+        pass
+        
---
+++ qal/nosql/xml-rpc.py
@@ -0,0 +1,20 @@
+"""
+Created on Oct 8, 2013
+
+@author: Nicklas Boerjesson
+"""
+
+from qal.nosql.custom import Custom_Dataset
+
+class XML_RPC_Dataset(Custom_Dataset):
+    """
+    This class implements querying data through XML-RPC
+    """
+    
+
+    def __init__(self, params):
+        """
+        Constructor
+        """
+        pass
+        
