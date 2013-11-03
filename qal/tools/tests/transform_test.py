3cd2cf3e8f7f0daaf51827f83cdd5c7c2ec4bca2
Nicklas <nibo@nibo01.(none)>
2013-11-04 00:12:56 +0100

    Create resources for testing and start testing structure

--- qal/tools/tests/transform_test.py
+++ qal/tools/tests/transform_test.py
@@ -4,13 +4,22 @@ Created on Nov 3, 2013
 @author: Nicklas Boerjesson
 """
 import unittest
-
+from qal.tools.transform import Trim
+from lxml import etree
 
 class Transform_test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
+    def test_trim(self):
+        """ Trim has no parameters"""
+        _tree = etree.ElementTree()
+        _tree.parse('test_merge_two_files.xml')
+        _tree.find("./resources/resource[@uuid='source_uuid']")
+        _trim = Trim()
+        _result = _trim.transform(' test ')
+        self.assertEqual(_result, 'test')
+        
+        
 
 
 if __name__ == "__main__":
