b83eff1c6ef2de81781c7940a649544fec7159b1
Nicklas <nibo@nibo01.(none)>
2013-11-05 22:03:21 +0100

    Add support for the Replace transformation and unify XML handling and testing

--- qal/tools/tests/transform_test.py
+++ qal/tools/tests/transform_test.py
@@ -4,22 +4,34 @@ Created on Nov 3, 2013
 @author: Nicklas Boerjesson
 """
 import unittest
-from qal.tools.transform import Trim
+from qal.tools.transform import Trim, Replace
 from lxml import etree
 
 class Transform_test(unittest.TestCase):
-
+    
+    def _parse_xml(self, _filename):
+        _parser = etree.XMLParser(remove_blank_text=True)
+        _tree = etree.ElementTree()
+        return _tree.parse(_filename, _parser)
+        
 
     def test_trim(self):
-        """ Trim has no parameters"""
-        _tree = etree.ElementTree()
-        _tree.parse('resources/test_merge_two_files.xml')
-        _trim_node = _tree.find("mappings/field_mappings/field_mapping/transformations/trim")
-        _trim = Trim(_trim_node)
-        _result = _trim.transform(' test ')
-        self.assertEqual(_result, ' test')
+        """Test trim transformation input/output and XML encoding/decoding"""
+        _tree = self._parse_xml('resources/test_merge_two_files.xml')
+        _xml_def = _tree.find("mappings/field_mappings/field_mapping/transformations/trim")
+        _tested = Trim(_xml_def)
+        _result = _tested.transform(' test ')
+        self.assertEqual(_result, ' test', "Results differ")
+        self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")
         
-        
+    def test_replace(self):
+        """Test replace transformation input/output and XML encoding/decoding"""
+        _tree = self._parse_xml('resources/test_merge_two_files.xml')
+        _xml_def = _tree.find("mappings/field_mappings/field_mapping/transformations/replace")
+        _tested = Replace(_xml_def)
+        _result = _tested.transform("unecccessary ccc")
+        self.assertEqual(_result, "uneccessary ccc", "Results differ")
+        self.assertEqual(etree.tostring(_xml_def, method="xml").strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")        
 
 
 if __name__ == "__main__":
--- qal/tools/transform.py
+++ qal/tools/transform.py
@@ -3,24 +3,45 @@ Created on Nov 3, 2013
 
 @author: Nicklas Boerjesson
 """
+from lxml import etree
+
+
+def isnone( _node):
+    if _node == None or _node.text == None:
+        return ''
+    else:
+        return _node.text  
 
 class Custom_Transformation(object):
     """
     This is the base class for all transformations
     """
-
+    
+    """Order dictates when the transformation is run."""
+    order = None
+    
     def __init__(self, _xml_node = None):
+        
         """
         Constructor
         """
         
         if _xml_node != None:
-
             self.load_from_xml_node(_xml_node)
+            
+    def init_base_to_node(self, _name):
+        _xml_node = etree.Element(_name)
+        _xml_node.set("order", self.order)
+        return _xml_node
+            
+    def as_xml_node(self):
+        raise Exception("Custom_Transformation.as_xml_node : Should not be called. Not implemented in base class, use init_base_to_node().")      
         
-        
-    def load_from_xml_node(self, _xml_node):
-        raise Exception("Custom_Transformation.load_from_xml_node : Not implemented in base class.")    
+    def load_from_xml_node(self, _xml_node = None):
+        if _xml_node != None:
+            self.order = _xml_node.get("order")
+        else:
+            raise Exception("Custom_Transformation.load_from_xml_node : Base class need a destination node.")  
     
     def transform(self, _value):
         """Make transformation"""
@@ -28,18 +49,53 @@ class Custom_Transformation(object):
 
 
 class Trim(Custom_Transformation):
-    leftright = None
+    """Trim returns a copy of the string in which all chars have been trimmed from the beginning and the end of the string (default whitespace characters).
+    If the beginning_end parameter is set to either "beginning" or "end", only the left or right end of the string is trimmed, respectively."""
+    beginning_end = None
     
     def load_from_xml_node(self, _xml_node):
-        self.leftright = _xml_node.text
+        super(Trim, self ).load_from_xml_node(_xml_node)
+        self.beginning_end = _xml_node.text
+    
+    def as_xml_node(self):
+        _xml_node = self.init_base_to_node("trim")
+        _xml_node.text = self.beginning_end
+        
+        return _xml_node
 
     def transform(self, _value):
         """Make transformation"""
-        if self.leftright == "left":
+        if self.beginning_end == "beginning":
             return _value.lstrip()
-        elif self.leftright =="right":
+        elif self.beginning_end =="end":
             return _value.rstrip()
         else:
             return _value.strip()
+
+class Replace(Custom_Transformation):
+    """Replace returns a copy of the string in which the occurrences of old have been replaced with new, optionally restricting the number of replacements to max."""
+    old = None
+    new = None
+    max = None
+    
+    def load_from_xml_node(self, _xml_node):
+        super(Replace, self ).load_from_xml_node(_xml_node)
+        self.old = isnone(_xml_node.find("old"))
+        self.new = isnone(_xml_node.find("new"))
+        self.max = isnone(_xml_node.find("max"))
+    
+    def as_xml_node(self):
+        _xml_node = self.init_base_to_node("replace")
+        etree.SubElement(_xml_node, "old").text = self.old
+        etree.SubElement(_xml_node, "new").text = self.new
+        etree.SubElement(_xml_node, "max").text = self.max
         
-        raise Exception("Custom_Transformation.transform : Not implemented in base class.")           
+        return _xml_node
+
+    def transform(self, _value):
+        """Make transformation"""
+        if self.max:
+            return _value.replace(self.old, self.new, int(self.max))
+        else:
+            return _value.replace(self.old, self.new)
+            
