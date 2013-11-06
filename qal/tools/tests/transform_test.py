917a0c6d04d9bb0ef1d215c516bf19c503e73b48
Nicklas <nibo@nibo01.(none)>
2013-11-06 22:06:13 +0100

    Add Is_null and Cast

--- qal/tools/tests/transform_test.py
+++ qal/tools/tests/transform_test.py
@@ -4,8 +4,9 @@ Created on Nov 3, 2013
 @author: Nicklas Boerjesson
 """
 import unittest
-from qal.tools.transform import Trim, Replace
+from qal.tools.transform import Trim, Replace, Is_null, Cast
 from lxml import etree
+from datetime import datetime
 
 class Transform_test(unittest.TestCase):
     
@@ -24,6 +25,15 @@ class Transform_test(unittest.TestCase):
         self.assertEqual(_result, ' test', "Results differ")
         self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")
         
+    def test_is_null(self):
+        """Test trim transformation input/output and XML encoding/decoding"""
+        _tree = self._parse_xml('resources/test_merge_two_files.xml')
+        _xml_def = _tree.find("mappings/field_mappings/field_mapping/transformations/is_null")
+        _tested = Is_null(_xml_def)
+        _result = _tested.transform(None)
+        self.assertEqual(_result, 'NULL', "Results differ")
+        self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")
+        
     def test_replace(self):
         """Test replace transformation input/output and XML encoding/decoding"""
         _tree = self._parse_xml('resources/test_merge_two_files.xml')
@@ -31,7 +41,16 @@ class Transform_test(unittest.TestCase):
         _tested = Replace(_xml_def)
         _result = _tested.transform("unneccessary cc")
         self.assertEqual(_result, "unnecessary cc", "Results differ")
-        self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")        
+        self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")  
+        
+    def test_cast(self):
+        """Test trim transformation input/output and XML encoding/decoding"""
+        _tree = self._parse_xml('resources/test_merge_two_files.xml')
+        _xml_def = _tree.find("mappings/field_mappings/field_mapping/transformations/cast")
+        _tested = Cast(_xml_def)
+        _result = _tested.transform("02/01/2010 00:00:01")
+        self.assertEqual(_result, datetime.strptime("2010-01-02 00:00:01", "%Y-%m-%d %H:%M:%S"), "Results differ")
+        self.assertEqual(etree.tostring(_xml_def).strip(), etree.tostring(_tested.as_xml_node()), "XML in/out differ")      
 
 
 if __name__ == "__main__":
--- qal/tools/transform.py
+++ qal/tools/transform.py
@@ -4,6 +4,8 @@ Created on Nov 3, 2013
 @author: Nicklas Boerjesson
 """
 from lxml import etree
+from datetime import date, datetime
+
 
 
 def isnone( _node):
@@ -50,27 +52,86 @@ class Custom_Transformation(object):
 
 class Trim(Custom_Transformation):
     """Trim returns a copy of the string in which all chars have been trimmed from the beginning and the end of the string (default whitespace characters).
-    If the beginning_end parameter is set to either "beginning" or "end", only the left or right end of the string is trimmed, respectively."""
-    beginning_end = None
+    If the value parameter is set to either "beginning" or "end", only the left or right end of the string is trimmed, respectively."""
+    value = None
     
     def load_from_xml_node(self, _xml_node):
         super(Trim, self ).load_from_xml_node(_xml_node)
-        self.beginning_end = _xml_node.text
+        self.value = _xml_node.text
     
     def as_xml_node(self):
         _xml_node = self.init_base_to_node("trim")
-        _xml_node.text = self.beginning_end
+        _xml_node.text = self.value
         
         return _xml_node
 
     def transform(self, _value):
         """Make transformation"""
-        if self.beginning_end == "beginning":
+        if self.value == "beginning":
             return _value.lstrip()
-        elif self.beginning_end =="end":
+        elif self.value =="end":
             return _value.rstrip()
         else:
             return _value.strip()
+        
+class Is_null(Custom_Transformation):
+    """Is_null returns a specified value if the input value is NULL."""
+    value = None
+    
+    def load_from_xml_node(self, _xml_node):
+        super(Is_null, self ).load_from_xml_node(_xml_node)
+        self.value = _xml_node.text
+    
+    def as_xml_node(self):
+        _xml_node = self.init_base_to_node("is_null")
+        _xml_node.text = self.value
+        
+        return _xml_node
+
+    def transform(self, _value):
+        """Make transformation"""
+        if _value == None:
+            return self.value
+        else:
+            return _value
+        
+class Cast(Custom_Transformation):
+    """ICasts a string to the specified type. The timestamp date format defaults to the ISO format if format_string is not set.\n
+    Possible format string directives at : http://docs.python.org/3.2/library/datetime.html#strftime-strptime-behavior\n
+    For example, 2013-11-06 22:05:42 is "%Y-%m-%d %H:%M:%S".
+    """
+    
+    dest_type = None
+    format_string = None
+    
+    def load_from_xml_node(self, _xml_node):
+        super(Cast, self ).load_from_xml_node(_xml_node)
+        self.dest_type = isnone(_xml_node.find("dest_type"))
+        self.format_string = isnone(_xml_node.find("format_string"))
+    
+    def as_xml_node(self):
+        _xml_node = self.init_base_to_node("cast")
+        etree.SubElement(_xml_node, "dest_type").text = self.dest_type
+        etree.SubElement(_xml_node, "format_string").text = self.format_string
+        
+        return _xml_node
+
+    def transform(self, _value):
+        """Make cast"""
+        if self.dest_type in ['string', 'string(255)', 'string(3000)']:
+            return str(_value)
+        elif self.dest_type in ['float']:
+            return float(_value)
+        elif self.dest_type in ['integer', 'serial']:
+            return int(_value)
+        elif self.dest_type in ['timestamp']:
+            if self.format_string != None:
+                
+                return datetime.strptime(_value, self.format_string)
+            else:
+                return datetime.strptime(_value, "%Y-%m-%d %H:%M:%S")
+        elif self.dest_type in ['boolean']:
+            return bool(_value)
 
 class Replace(Custom_Transformation):
     """Replace returns a copy of the string in which the occurrences of old have been replaced with new, optionally restricting the number of replacements to max."""
