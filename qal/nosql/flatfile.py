37791bf95c5719aba4eb1df5160c3969c07149b2
Nicklas <nibo@nibo01.(none)>
2013-11-08 00:25:30 +0100

    Add support for quoting

--- qal/nosql/flatfile.py
+++ qal/nosql/flatfile.py
@@ -19,7 +19,7 @@ class Flatfile_Dataset(Custom_Dataset):
     csv_dialect = None
 #    field_names = None
     
-    def __init__(self, _delimiter = None, _filename = None, _has_header = None, _csv_dialect = None, _resource = None):
+    def __init__(self, _delimiter = None, _filename = None, _has_header = None, _csv_dialect = None, _resource = None, _quoting = None):
         """Constructor"""
         super(Flatfile_Dataset, self ).__init__() 
        
@@ -44,6 +44,10 @@ class Flatfile_Dataset(Custom_Dataset):
             else:
                 self.csv_dialect = None      
              
+            if _quoting != None: 
+                self.quoting = _quoting
+            else:
+                self.quoting = None      
 
         
     def read_resource_settings(self, _resource):
@@ -53,6 +57,15 @@ class Flatfile_Dataset(Custom_Dataset):
         self.delimiter =   _resource.data.get("delimiter")
         self.has_header =  bool(_resource.data.get("has_header"))
         self.csv_dialect = _resource.data.get("csv_dialect")
+        self.quoting    = _resource.data.get("quoting")
+    
+    def _quotestr_to_constants(self, _str):
+        if _str == "MINIMAL":
+            return csv.QUOTE_MINIMAL
+        elif _str == "ALL":
+            return csv.QUOTE_ALL
+        else:
+            return csv.QUOTE_NONE
 
     def load(self):
         """Load data"""
@@ -60,7 +73,7 @@ class Flatfile_Dataset(Custom_Dataset):
         print("Flatfile_Dataset.load: Filename='"+str(os.path.normpath(_tmp_dir_abs +'/' + self.filename)) + "', Delimiter='"+str(self.delimiter)+"'")
         
         _file = open(os.path.normpath(_tmp_dir_abs +'/' + self.filename), 'r')
-        _reader = csv.reader(_file, delimiter=self.delimiter, quoting=csv.QUOTE_NONE)
+        _reader = csv.reader(_file, delimiter=self.delimiter, quoting=self._quotestr_to_constants(self.quoting))
         _first_row = True
         self.data_table = []
         for _row in _reader:
