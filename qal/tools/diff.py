d5f4ce4c883de792e29789b919a8be98e27b087b
Nicklas <nibo@nibo01.(none)>
2013-11-08 00:26:16 +0100

    Implement 2d matrix diff

--- qal/tools/diff.py
+++ qal/tools/diff.py
@@ -7,9 +7,23 @@ GNU diffutils does for files, but with datasets.
 
 """
 
+def cmp_key_columns(_left, _right, _key_columns):
+    for _curr_key_column in _key_columns:
+        if _left[_curr_key_column] < _right[_curr_key_column]:
+            return -1
+        elif _left[_curr_key_column] > _right[_curr_key_column]:
+            return 1
 
+    return 0
 
-def compare(self, _left, _right, _key_columns, _full):
+def match_all_columns(_left, _right):
+    for _curr_column in range(1, len(_right)):
+        if _left[_curr_column] != _right[_curr_column]:
+            return False
+
+    return True   
+
+def compare(_left, _right, _key_columns, _full):
     """ The compare function takes two structurally identical 2-dimensional matrices,
         _left and _right, matches them using the columns in _key_colums,
         and returns a tuple of the results.
@@ -21,18 +35,73 @@ def compare(self, _left, _right, _key_columns, _full):
         and the third result, _different is populated with a list of rows
         where the values differ.
     """    
-    _missing_left = _missing_right = _difference = []
-    
+    _missing_left = []
+    _missing_right = []
+    _difference = []
+
     # Order _left and _right using key columns
-    
-    
-    # From top, loop data sets, compare all rows
-    
-        # Keys differ, compare _right with next row from _left to see who is missing
+    if len(_key_columns) == 1:   
+        _left_s = sorted(_left, key=lambda d: ( d[_key_columns[0]]))
+        _right_s = sorted(_right, key=lambda d: ( d[_key_columns[0]]))
+    elif len(_key_columns) == 2:   
+        _left_s = sorted(_left, key=lambda d: ( d[_key_columns[0]], d[_key_columns[1]]))
+        _right_s = sorted(_right, key=lambda d: ( d[_key_columns[0]], d[_key_columns[1]]))
+    elif len(_key_columns) == 3:   
+        _left_s = sorted(_left, key=lambda d: ( d[_key_columns[0]], d[_key_columns[1]], d[_key_columns[2]]))
+        _right_s = sorted(_right, key=lambda d: ( d[_key_columns[0]], d[_key_columns[1]], d[_key_columns[2]]))
+    else:
+        raise Exception("Err..sorry, only 3 key columns are supported currently, too tired to make it dynamic. :-)")
         
-            # Keys for _left[+1] = _right, add row to _missing_left
+    # From top, loop data sets, compare all rows
+    _left_idx = _right_idx = 0
+    _left_len = len(_left_s)
+    _right_len = len(_right_s)
+    while _left_idx < _left_len and _right_idx < _right_len:
+        print("_left_idx :" + str(_left_idx) + " value: "+str(_left_s[_left_idx][_key_columns[0]]) + " | _right_idx : "  + str(_right_idx)+ " value: "+str(_right_s[_right_idx][_key_columns[0]]))
+        _cmp_res = cmp_key_columns(_left_s[_left_idx], _right_s[_right_idx], _key_columns)
+        print("_cmp_res :" + str(_cmp_res))
+        if _cmp_res < 0:
+            print("_missing_right.append " + str(_left_s[_left_idx]))
+            _missing_right.append(_left_s[_left_idx])
+            _left_idx+= 1
+        elif _cmp_res > 0:
+            print("_missing_left.append " + str(_right_s[_right_idx]))
+            _missing_left.append(_right_s[_right_idx])            
+            _right_idx+= 1
+        else:
+            if _full == True:
+                if match_all_columns(_left_s[_left_idx], _right_s[_right_idx]) != True:
+                    _difference.append([_left_s[_left_idx], _right_s[_right_idx], _left_idx, _right_idx])
+            _left_idx+= 1
+            _right_idx+= 1
             
-            # Keys for _left[+1] != _right, add row to _missing_right
+            
+                #===============================================================
+                # # Keys differ, compare _right with next row from _left to see who is missing
+                # if _left_len < _left_idx + 1:
+                #     _missing_left.append(_right[_right_idx])
+                # # The next left matches
+                # elif (match_key_columns(_left[_left_idx +1], _right[_left_idx], _key_columns) == False):
+                #     _missing_left.append(_right[_right_idx])
+                #     _right_idx+=1
+                # else:
+                #     # Keys for _left[+1] = _right, add row to _missing_left
+                #     _missing_right.append(_left[_right_idx])
+                #     _left_idx+=1
+                #===============================================================
+                    
+        
+
+            
+    # Add remainders to missing
+    if _left_idx < _left_len:
+        print("_missing_right.append (post) " + str(_left_s[_left_idx: _left_len]))
+        _missing_right+= _left_s[_left_idx: _left_len]
+    if _right_idx < _right_len:
+        print("_missing_left.append (post)" + str(_right_s[_right_idx: _right_len]))
+        _missing_left+=_right_s[_right_idx: _right_len]
+                
+            
             
         # Keys are the same and _full is set, check all data 
             
--- qal/tools/tests/diff_test.py
+++ qal/tools/tests/diff_test.py
@@ -4,14 +4,22 @@ Created on Nov 3, 2013
 @author: Nicklas Boerjesson
 """
 import unittest
-
+from qal.nosql.flatfile import Flatfile_Dataset
+from qal.tools.diff import compare
 
 class Diff_test(unittest.TestCase):
 
 
     def testName(self):
-        pass
-
+        _ff_source = Flatfile_Dataset(_filename = "resources/csv_source.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL")
+        _dataset_source = _ff_source.load()
+        _ff_dest = Flatfile_Dataset(_filename = "resources/csv_dest.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL")
+        _dataset_dest = _ff_dest.load()
+        print(str(_dataset_dest))
+        _missing_left, _missing_right, _difference = compare(_dataset_source, _dataset_dest, [0], True)
+        print(str(_missing_left))
+        print(str(_missing_right))
+        print(str(_difference))
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
