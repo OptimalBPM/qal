99273a636003a93708e759affc9ad8a11ddd0eff
Nicklas <nibo@nibo01.(none)>
2013-12-19 23:05:31 +0100

    Implemented save functionality and tests for Flatfile_Dataset

--- qal/nosql/flatfile.py
+++ qal/nosql/flatfile.py
@@ -8,7 +8,6 @@ Created on Sep 14, 2012
 from qal.nosql.custom import Custom_Dataset
 
 import csv
-import os
 
 class Flatfile_Dataset(Custom_Dataset):
  
@@ -59,22 +58,35 @@ class Flatfile_Dataset(Custom_Dataset):
         self.has_header =  bool(_resource.data.get("has_header"))
         self.csv_dialect = _resource.data.get("csv_dialect")
         self.quoting    = _resource.data.get("quoting")
-    
+        self.escapechar    = _resource.data.get("escapechar")
+        self.lineterminator = _resource.data.get("lineterminator")
+        self.quotechar = _resource.data.get("quotechar") or '"'       
+        self.skipinitialspace = _resource.data.get("skipinitialspace")
+         
     def _quotestr_to_constants(self, _str):
-        if _str == "MINIMAL":
-            return csv.QUOTE_MINIMAL
-        elif _str == "ALL":
-            return csv.QUOTE_ALL
-        else:
+        if _str == None:
             return csv.QUOTE_NONE
+        elif _str.upper() == "MINIMAL":
+            return csv.QUOTE_MINIMAL
+        elif _str.upper() == "ALL":
+            return csv.QUOTE_ALL
+        elif _str.upper() == "NONNUMERIC":
+            return csv.QUOTE_NONNUMERIC
+        else:
+            raise Exception("Error in _quotestr_to_constants: " + str(_str) + " is an invalid quotestr.")
+            
 
     def load(self):
         """Load data"""
-        _tmp_dir_abs = os.getcwd() 
         print("Flatfile_Dataset.load: Filename='" + str(self.filename) + "', Delimiter='"+str(self.delimiter)+"'")
         
         _file = open(self.filename, 'r')
-        _reader = csv.reader(_file, delimiter=self.delimiter, quoting=self._quotestr_to_constants(self.quoting))
+        _reader = csv.reader(_file, 
+                             delimiter=self.delimiter, 
+                             quoting=self._quotestr_to_constants(self.quoting),
+                             quotechar = self.quotechar,
+                             skipinitialspace = self.skipinitialspace
+                             )
         _first_row = True
         self.data_table = []
         for _row in _reader:
@@ -95,29 +107,40 @@ class Flatfile_Dataset(Custom_Dataset):
         return self.data_table   
     
     
-    def save(self):
+    def save(self, _save_as = None):
         """Save data"""
-        _tmp_dir_abs = os.getcwd() 
-        print("Flatfile_Dataset.load: Filename='" + str(self.filename) + "', Delimiter='"+str(self.delimiter)+"'")
+
+        print("Flatfile_Dataset.save: Filename='" + str(self.filename) + "', Delimiter='"+str(self.delimiter)+"'")
         
-        _file = open(self.filename, 'r')
-        _reader = csv.reader(_file, delimiter=self.delimiter, quoting=self._quotestr_to_constants(self.quoting))
-        _first_row = True
-        self.data_table = []
-        for _row in _reader:
-            # Save header row if existing.
-            if (_first_row and self.has_header == True):
-                self.field_names = [_curr_col.replace("'", "").replace("\"", "") for _curr_col in _row]
-                print("self.field_names :" + str(self.field_names))
-                _first_row = False
-            else:
-                self.data_table.append(_row)
-                    
+        if _save_as:
+            _filename = _save_as
+        else:
+            _filename = self.filename
             
-        _file.close()    
-        if (self.has_header == False):
-            for _curr_idx in range(0,len(_reader[0])):
-                self.field_names.append("Field_"+ str(_curr_idx))   
+        _file = open(_filename, 'w')
+
+        # Work around silly line terminator default, has to be set if passed.
+        if self.lineterminator:
+            _writer = csv.writer(_file,                              
+                             delimiter=self.delimiter, 
+                             quoting=self._quotestr_to_constants(self.quoting),
+                             quotechar = self.quotechar,
+                             skipinitialspace = self.skipinitialspace,
+                             lineterminator = self.lineterminator
+                             )
+        else:
+            _writer = csv.writer(_file,                              
+                             delimiter=self.delimiter, 
+                             quoting=self._quotestr_to_constants(self.quoting),
+                             quotechar = self.quotechar,
+                             skipinitialspace = self.skipinitialspace
+                             )
+
             
-        return self.data_table   
-       
+        if self.has_header == True:
+            _writer.writerow(self.field_names)
+    
+            
+        _writer.writerows(self.data_table)
+            
+        _file.close()
--- qal/nosql/tests/flatfile_test.py
+++ qal/nosql/tests/flatfile_test.py
@@ -1,19 +1,43 @@
-'''
+"""
 Created on Dec 17, 2013
 
-@author: nibo
-'''
+@author: Nicklas Boerjesson
+"""
+
 import unittest
 
+from qal.nosql.flatfile import Flatfile_Dataset
+from qal.common.resources import Resources
+from lxml import etree
+from qal.tools.diff import diff_files
 import os
 Test_Script_Dir = os.path.dirname(__file__)
 Test_Resource_Dir = Test_Script_Dir + '/resources'
 
+def load_xml(_filename):
+    return etree.parse(_filename)
+
 class Test(unittest.TestCase):
 
 
-    def testName(self):
-        pass
+    def test_1_Load_Save(self):
+        _resources_node = load_xml(Test_Resource_Dir + "/file_resources.xml").find("resources")
+        _resources = Resources(_resources_node = _resources_node)
+        _da = Flatfile_Dataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22FE}"))
+        _da.load()
+        print(str(_da.data_table))
+        _da.save(_save_as = Test_Resource_Dir + "/csv_out.csv")
+        
+        _f_a = open(Test_Resource_Dir + "/csv_out.csv", "r")
+        _f_b = open(Test_Resource_Dir + "/csv_cmp.csv", "r")
+        _a = _f_a.read()
+        _b = _f_b.read()
+        _f_a.close()
+        _f_b.close()
+        self.assertEqual(_a, _b, "test_1_Load_Save: Files are not equal")
+        
+        
+        
 
 
 if __name__ == "__main__":
