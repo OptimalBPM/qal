38944ea3edad294ca1ec72420601ee0b2fbfa041
Nicklas <nibo@nibo01.(none)>
2013-12-19 20:39:09 +0100

    Correct tests after bug fix

--- qal/tools/tests/diff_test.py
+++ qal/tools/tests/diff_test.py
@@ -24,16 +24,20 @@ class Diff_test(unittest.TestCase):
         #print(str(_dataset_dest))
         _missing_left, _missing_right, _difference, _sorted = compare(_dataset_source, _dataset_dest, [0], True)
         print(str(_missing_left))
-        self.assertEqual(_missing_left, [[1, 12, ['7999', 'BORJESSON', 'HACKER', '7839', '2013-01-01', '99999', '', '10']]], 'Missing left differs')
+        print(str(_missing_right))
+        print(str(_difference))
+        self.assertEqual(_missing_left, [[9, 7, ['7844', 'TURNER', 'SALESMAN', '7698', '1981-09-08 00:00:00', '1500', '', '30']], 
+                                         [2, 12, ['7999', 'BORJESSON', 'HACKER', '7839', '2013-01-01', '99999', '', '10']]], 'Missing left differs')
         self.assertEqual(_missing_right, [[6,6,['7782', 'CLARK', 'MANAGER', '7839', '1981-06-09 00:00:00', '2450', '', '10']], 
                                           [7,6,['7788', 'SCOTT', 'ANALYST', '7566', '1982-12-09 00:00:00', '3000', '', '20']]], 'Missing right differs')
-        self.assertEqual(_difference, [[0, 0,['7369', 'SMITH', 'CLERK', '7902', '1980-12-17 00:00:00', '800', '', '20'], 
+        self.assertEqual(_difference, [[0, 0, ['7369', 'SMITH', 'CLERK', '7902', '1980-12-17 00:00:00', '800', '', '20'], 
                                         ['7369', 'SMITH', 'CLERK', '7902', '1980-12-17 00:00:00', '700', '', '20']], 
-                                       [8, 6,['7839', 'KING', 'PRESIDENT ', '', '1981-11-17 00:00:00', '5000', '', '10'], 
+                                       [8, 6, ['7839', 'KING', 'PRESIDENT ', '', '1981-11-17 00:00:00', '5000', '', '10'], 
                                         ['7839', 'KING', 'PRESIDENT', '', '1981-11-17 00:00:00', '4500', '', '10']], 
-                                       [10, 8,['7876', 'ADAMS', 'CLERK', '7788', '1983-01-12 00:00:00', '1100,5', '', '20'], 
-                                        ['7876', 'ADAMS', 'CLERK', '7788', '1983-01-12 00:00:00', '1100', '', '20']]]
-                                        , 'Difference differs')
+                                       [9, 8, ['7876', 'ADAMS', 'CLERK', '7788', '1983-01-12 00:00:00', '1100,5', '', '20'], 
+                                        ['7876', 'ADAMS', 'CLERK', '7788', '1983-01-12 00:00:00', '1100', '', '20']]
+                                        ], 
+                                       'Difference differs')
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
