2a26605da55ac5fafd2d0a8484cef5e346387782
Nicklas <nibo@nibo01.(none)>
2014-01-02 01:19:10 +0100

    Add __version__, __release__ and __copyright to satisfy sphinx

--- qal/__init__.py
+++ qal/__init__.py
@@ -4,6 +4,9 @@ The Query Abstraction Layer(QAL) package.
 *****************************************
 
 QAL is designed to be a general library of data and database management functions.
- 
 
 """
+# Version and release information for sphinx documentation
+__version__ = '0.2'
+__release__ = '0.2.0'
+__copyright__ = '2010-2013, Nicklas Börjesson'
