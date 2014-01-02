ce39d3de4675bc385cc5b6b9937eb7b7b0be2c36
Nicklas <nibo@nibo01.(none)>
2014-01-02 23:17:36 +0100

    Fix setup for sphinx compatibility

--- setup.py
+++ setup.py
@@ -9,23 +9,23 @@ Created on Aug 11, 2013
 
 from setuptools import setup
 from qal import __release__
-
-setup(
-    name='qal',
-    version=__release__,
-    description='QAL is a Python library for mixing data sources into SQL statements.',
-    author='Nicklas Boerjesson',
-    author_email='nicklas_attheold_optimalbpm.se',
-    maintainer='Nicklas Boerjesson',
-    maintainer_email='nicklas_attheold_optimalbpm.se',
-    long_description="""Query Abstraction Layer is a Python library for mixing data from different data sources into SQL statements.\
-      It supports several database backends and file formats.
-      """,
-    url='https://sourceforge.net/projects/qal/',
-    packages=['qal', 'qal.dal', 'qal.dal.tests', 'qal.sql', 'qal.sql.tests', 'qal.sql.tests.resources', 
-              'qal.common', 'qal.dataset', 'qal.tools', 'qal.tools.tests'],
-    package_data = {
-        # If any package contains *.txt or *.xml files, include them:
-        '': ['*.txt', '*.xml', '*.sql']
-    },
-    license='BSD')
+if __name__ == "__main__":
+    setup(
+        name='qal',
+        version=__release__,
+        description='QAL is a Python library for mixing data sources into SQL statements.',
+        author='Nicklas Boerjesson',
+        author_email='nicklas_attheold_optimalbpm.se',
+        maintainer='Nicklas Boerjesson',
+        maintainer_email='nicklas_attheold_optimalbpm.se',
+        long_description="""Query Abstraction Layer is a Python library for mixing data from different data sources into SQL statements.\
+          It supports several database backends and file formats.
+          """,
+        url='https://sourceforge.net/projects/qal/',
+        packages=['qal', 'qal.dal', 'qal.dal.tests', 'qal.sql', 'qal.sql.tests', 'qal.sql.tests.resources', 
+                  'qal.common', 'qal.dataset', 'qal.tools', 'qal.tools.tests'],
+        package_data = {
+            # If any package contains *.txt or *.xml files, include them:
+            '': ['*.txt', '*.xml', '*.sql']
+        },
+        license='BSD')
