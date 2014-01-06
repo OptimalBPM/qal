a6b2ab48f6f88bd8aea264cb955362f187781d46
Nicklas <nibo@nibo01.(none)>
2014-01-06 03:47:37 +0100

    Begin implementation of Spreadsheet_dataset

--- qal/dataset/spreadsheet.py
+++ qal/dataset/spreadsheet.py
@@ -7,9 +7,12 @@ Created on Dec 28, 2013
 
 from qal.dataset.custom import Custom_Dataset
 
+import os
+from xlrd import Workbook
+
 class Spreadsheet_Dataset(Custom_Dataset):
  
-    """The Spreadsheet dataset holds a spreadsheet"""
+    """The Spreadsheet dataset holds a spreadsheet, currently only XLS."""
     
     _dal = None
     
@@ -23,10 +26,29 @@ class Spreadsheet_Dataset(Custom_Dataset):
         
     def load(self):
         """Load data. Not implemented as it is not needed in the matrix descendant"""
+
+        import os.path
+        _extension = os.path.splitext(self.filename)[1]
+        
+        #TODO: Check file type
+        if _extension.lower() in ["xls", "xlsx"]:
+            from xlrd import open_workbook
+            _workbook = open_workbook('my_workbook.xls')
+        elif _extension.lower() == "odt":
+            from ezodf import newdoc, Paragraph, Heading, Sheet
+            raise Exception("Spreadsheet_Dataset.load: Open Document Spreadsheet not implemented yet.")
+
+        else:
+            raise Exception("Spreadsheet_Dataset.load: Unsupported file type \"" + _extension +"\"")
+            
+        pass
+        
         pass
     
     def save (self):
         """Save data. Not implemented as it is not needed in the matrix descendant"""
-        pass
+        
+        
+
     
         
