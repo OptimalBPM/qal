a0e3e4cbf28638be2167048f0ea7539074de1ec3
Nicklas <nibo@nibo01.(none)>
2014-01-06 14:17:20 +0100

    Implement reading excel files

--- qal/dataset/spreadsheet.py
+++ qal/dataset/spreadsheet.py
@@ -13,15 +13,56 @@ class Spreadsheet_Dataset(Custom_Dataset):
  
     """The Spreadsheet dataset holds a spreadsheet, currently only XLS."""
     
-    _dal = None
+    has_header = None
+    """True if data begins with a header row containing field names."""
+    filename = None
+    """The name of the file."""
+    field_names = None
+    """The names of the fields(if applicable, see has_header)."""
+    sheet_name = None
+    """The name of the worksheet to use, typically "Sheet1"."""
     
-    def __init__(self):
+    
+    def read_resource_settings(self, _resource):
+        if _resource.type.upper() != 'SPREADSHEET':
+            raise Exception("Spreadsheet_Dataset.read_resource_settings.parse_resource error: Wrong resource type: " + _resource.type)
+        self.filename =    _resource.data.get("filename")
+        self.delimiter =   _resource.data.get("delimiter")
+        self.has_header =  bool(_resource.data.get("has_header"))
+        self.sheet_name = _resource.data.get("sheet_name")
+        self.x_offset = _resource.data.get("x_offset")
+        self.y_offset = _resource.data.get("y_offset")
+    
+    def __init__(self, _filename = None, _has_header = None, _resource = None, _sheet_name = None, _x_offset = None, _y_offset = None):
         '''
         Constructor
         '''
         super(Spreadsheet_Dataset, self ).__init__()
-        
-        
+        if _resource != None:
+            self.read_resource_settings(_resource)        
+        else:
+  
+            if _filename != None: 
+                self.filename = _filename
+            else:
+                self.filename = None      
+            if _has_header != None: 
+                self.has_header = _has_header
+            else:
+                self.has_header = None
+            if _sheet_name != None: 
+                self.sheet_name = _sheet_name
+            else:
+                self.sheet_name = None
+            if _x_offset != None: 
+                self.x_offset = _x_offset
+            else:
+                self.x_offset = None
+            if _y_offset != None: 
+                self.y_offset = _y_offset
+            else:
+                self.y_offset = None
+                  
         
     def load(self):
         """Load data. Not implemented as it is not needed in the matrix descendant"""
@@ -30,22 +71,41 @@ class Spreadsheet_Dataset(Custom_Dataset):
         _extension = os.path.splitext(self.filename)[1]
         
         #TODO: Check file type
-        if _extension.lower() in ["xls", "xlsx"]:
+        if _extension.lower() in [".xls", ".xlsx"]:
             from xlrd import open_workbook
-            _workbook = open_workbook('my_workbook.xls')
-        elif _extension.lower() == "odt":
+            try:
+                _workbook = open_workbook(self.filename)
+                _sheet = _workbook.sheet_by_name(self.sheet_name)
+                if self.x_offset is None:
+                    self.x_offset = 0
+                    
+                if self.y_offset is None:
+                    self.y_offset = 1
+              
+                if self.has_header:
+                    self.field_names = _sheet.row_values(self.y_offset, start_colx=self.x_offset, end_colx= _sheet.ncols)
+                    _has_header_offset = 1
+                else:
+                    _has_header_offset = 0
+
+                self.data_table = []
+
+                for _curr_y in range(self.y_offset + _has_header_offset, _sheet.nrows):
+                    self.data_table.append(_sheet.row_values(_curr_y, start_colx=0, end_colx= _sheet.ncols))
+
+            except IOError as e:
+                raise Exception("Spreadsheet_Dataset.load: Error reading file:" + str(e))
+            
+            
+        elif _extension.lower() == ".odt":
             from ezodf import newdoc, Paragraph, Heading, Sheet
             raise Exception("Spreadsheet_Dataset.load: Open Document Spreadsheet not implemented yet.")
 
         else:
             raise Exception("Spreadsheet_Dataset.load: Unsupported file type \"" + _extension +"\"")
-            
-        pass
-        
-        pass
     
     def save (self):
-        """Save data. Not implemented as it is not needed in the matrix descendant"""
+        """Save data. Not implemented yet"""
         
         
 
---
+++ qal/dataset/tests/spreadsheet_test.py
@@ -0,0 +1,47 @@
+"""
+Created on Dec 17, 2013
+
+@author: Nicklas Boerjesson
+"""
+
+import unittest
+
+from qal.dataset.flatfile import Flatfile_Dataset
+from qal.common.resources import Resources
+from lxml import etree
+from qal.tools.diff import diff_files
+from qal.dataset.spreadsheet import Spreadsheet_Dataset
+
+import os
+Test_Script_Dir = os.path.dirname(__file__)
+Test_Resource_Dir = Test_Script_Dir + '/resources'
+
+def load_xml(_filename):
+    return etree.parse(_filename)
+
+class Test(unittest.TestCase):
+
+
+    def test_1_Load_Save(self, _has_header = None, _resource = None):
+        _resources_node = load_xml(Test_Resource_Dir + "/resources.xml").find("resources")
+        _resources = Resources(_resources_node = _resources_node)
+        _da = Spreadsheet_Dataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22A1}"))
+        _da.load()
+        print(str(_da.data_table))
+        _da.save(_save_as = Test_Resource_Dir + "/excel_out.csv")
+        
+        _f_a = open(Test_Resource_Dir + "/excel_out.csv", "r")
+        _f_b = open(Test_Resource_Dir + "/excel_cmp.csv", "r")
+        _a = _f_a.read()
+        _b = _f_b.read()
+        _f_a.close()
+        _f_b.close()
+        self.assertEqual(_a, _b, "test_1_Load_Save: Files are not equal")
+        
+        
+        
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
