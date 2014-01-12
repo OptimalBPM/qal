9e108ab3545ec443b3c44fda4b4def527e9e2e43
Nicklas <nibo@nibo01.(none)>
2014-01-12 22:28:24 +0100

    Add logging

--- qal/dataset/tests/flatfile_test.py
+++ qal/dataset/tests/flatfile_test.py
@@ -8,8 +8,9 @@ import unittest
 
 from qal.dataset.flatfile import Flatfile_Dataset
 from qal.common.resources import Resources
+from qal.common.listhelper import pretty_list
 from lxml import etree
-from qal.tools.diff import diff_files
+from qal.dataset.custom import DATASET_LOGLEVEL_DETAIL
 import os
 Test_Script_Dir = os.path.dirname(__file__)
 Test_Resource_Dir = Test_Script_Dir + '/resources'
@@ -24,16 +25,19 @@ class Test(unittest.TestCase):
         _resources_node = load_xml(Test_Resource_Dir + "/resources.xml").find("resources")
         _resources = Resources(_resources_node = _resources_node)
         _da = Flatfile_Dataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22FE}"))
+        _da._log_level = DATASET_LOGLEVEL_DETAIL
         _da.load()
-        print(str(_da.data_table))
+        print("Source: " + pretty_list(_da.data_table))
         _da.save(_save_as = Test_Resource_Dir + "/csv_out.csv")
-        
+        print("Log: " + pretty_list(_da._log))
         _f_a = open(Test_Resource_Dir + "/csv_out.csv", "r")
         _f_b = open(Test_Resource_Dir + "/csv_cmp.csv", "r")
         _a = _f_a.read()
         _b = _f_b.read()
         _f_a.close()
         _f_b.close()
+        
+        
         self.assertEqual(_a, _b, "test_1_Load_Save: Files are not equal")
         
         
