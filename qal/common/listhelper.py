f9514651fb41b8a1a38f7596897e6909c8786635
Nicklas <nibo@nibo01.(none)>
2014-01-12 22:23:36 +0100

    Add pretty_array function

--- qal/common/listhelper.py
+++ qal/common/listhelper.py
@@ -31,3 +31,28 @@ def find_previous_match(_list, _start_idx, _match):
         if _list[_curr_idx] == _match:
             return _curr_idx
     return -1
+
+def pretty_array(_array):
+    """Returns proper array data representation syntax, but with each row on a new text row, to make it more readable and usable."""
+    
+    def _handle_types(_data):
+        if isinstance(_data, list):
+            return str(_data)
+        else:
+            if isinstance(_data, str):
+                return "['" + _data + "']"    
+            else:
+                return "[" + str(_data) + "]"       
+    
+    _result = ""
+    if len(_array) > 0:
+        for _row_idx in range(len(_array) - 2) :
+            _result+= _handle_types(_array[_row_idx]) + ",\n"
+
+        _result+= _handle_types(_array[_row_idx + 1]) + "\n"
+        return "[\n" + _result + "]"
+    else:
+        return "[]"
+    
+    
+        
