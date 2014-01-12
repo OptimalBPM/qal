ce852fa56e3a0d25b14bd0fbe597ff6a3a307e49
Nicklas <nibo@nibo01.(none)>
2014-01-12 11:54:48 +0100

    Generalize test and stage before each run

--- qal/dataset/tests/rdbms_test.py
+++ qal/dataset/tests/rdbms_test.py
@@ -5,11 +5,12 @@ Created on Dec 17, 2013
 """
 
 import unittest
-
+import datetime
 from qal.dataset.rdbms import RDBMS_Dataset
 from qal.dataset.custom import DATASET_LOGLEVEL_DETAIL
 from qal.common.resources import Resources
 from qal.dal.dal import Database_Abstraction_Layer
+from qal.sql.macros import copy_to_table
 from lxml import etree
 
 
@@ -30,41 +31,52 @@ class Test(unittest.TestCase):
         _resources_node = load_xml(Test_Resource_Dir + "/resources.xml").find("resources")
         _resources = Resources(_resources_node = _resources_node)
         
-        _d_pgsql = RDBMS_Dataset(_resource= _resources.get_resource("{1D62083E-88F7-4442-920D-0B6CC59BA2FF}"))
-        _d_pgsql.load()
-        print("source:\n" + str(_d_pgsql.data_table))
-
-        print("Staging destination (TODO: Add source staging as well!")
-        _staging_dal = Database_Abstraction_Layer(_resource= _resources.get_resource("{DD34A233-47A6-4C16-A26F-195711B49B97}"))
-        _staging_dal.execute("DROP TABLE table_mysql;")
-        _staging_dal.execute("CREATE TABLE table_mysql (table_mysqlID integer, table_mysqlName varchar(200), table_mysqlChanged timestamp);")
-        _staging_dal.execute("INSERT INTO table_mysql (table_mysqlID, table_mysqlName, table_mysqlChanged) VALUES (1, 'MySQL', '2001-01-01');")
-        _staging_dal.execute("INSERT INTO table_mysql (table_mysqlID, table_mysqlName, table_mysqlChanged) VALUES (2, 'MySQL', '2001-01-02');")
-        _staging_dal.execute("INSERT INTO table_mysql (table_mysqlID, table_mysqlName, table_mysqlChanged) VALUES (3, 'MySQL', '2001-01-04');")
+        print("rdbms_test.test_1_Load_Save: Staging source")
+        _source_data = [[1, 'source', datetime.datetime(2001, 1, 1, 0, 0)], [2, 'source', datetime.datetime(2001, 1, 2, 0, 0)], [3, 'source_new', datetime.datetime(2014, 1, 1, 0, 0)]]
+        _field_names = ["ID", "Name", "Changed"]
+        _field_types = ["integer", "string(200)", "timestamp"]
         
-        _staging_dal.commit()
-        _staging_dal.close()
+        _source_dal = Database_Abstraction_Layer(_resource= _resources.get_resource("{1D62083E-88F7-4442-920D-0B6CC59BA2FF}"))
+        _source_table_name = 'table_src'
+        copy_to_table(_source_dal, _source_data, _field_names, _field_types, _source_table_name, _create_table = True, _drop_existing = True)
         
-        _d_mysql = RDBMS_Dataset(_resource= _resources.get_resource("{DD34A233-47A6-4C16-A26F-195711B49B97}"))
-        _d_mysql._log_level = DATASET_LOGLEVEL_DETAIL
-        _d_mysql.load()
+        print("rdbms_test.test_1_Load_Save: Staging destination")
+        _dest_data = [[1, 'dest', datetime.datetime(2001, 1, 1, 0, 0)], [2, 'dest', datetime.datetime(2001, 1, 2, 0, 0)], [3, 'dest', datetime.datetime(2014, 1, 4, 0, 0)]]
+        
+        _dest_dal = Database_Abstraction_Layer(_resource= _resources.get_resource("{DD34A233-47A6-4C16-A26F-195711B49B97}"))
+        _dest_table_name = 'table_dst'
+        copy_to_table(_dest_dal, _dest_data, _field_names, _field_types, _dest_table_name, _create_table = True, _drop_existing = True)
+ 
+        _dest_dal.commit()
+        
+        #Load source
+        _d_source = RDBMS_Dataset(_resource= _resources.get_resource("{1D62083E-88F7-4442-920D-0B6CC59BA2FF}"))
+        _d_source.load()
+        print("source:\n" + str(_d_source.data_table))
 
-        print("dest:\n" + str(_d_mysql.data_table))
+        #Load dest        
+        _d_dest = RDBMS_Dataset(_resource= _resources.get_resource("{DD34A233-47A6-4C16-A26F-195711B49B97}"))
+        _d_dest._log_level = DATASET_LOGLEVEL_DETAIL
+        _d_dest.load()
+
+        print("dest:\n" + str(_d_dest.data_table))
                 
-        _d_mysql.apply_new_data(_d_pgsql.data_table, [2])
+        _d_dest.apply_new_data(_d_source.data_table, [2])
         
-        _d_mysql.save()
-        _d_mysql.load()
+        _d_dest.save()
+        _d_dest.load()
         
-        print(str(_d_mysql._log))
+        print("log:\n"+ str(_d_dest._log))
         
-        _d_pgsql.dal.close()
-        _d_mysql.dal.close()
-
-        #self.assertEqual(_a, _b, "test_1_Load_Save: Files are not equal")
+        _log_cmp = ['RDBMS_Dataset.delete;%5Bdatetime.datetime%282014%2C%201%2C%204%2C%200%2C%200%29%5D;%5B3%2C%20%27dest%27%2C%20datetime.datetime%282014%2C%201%2C%204%2C%200%2C%200%29%5D;Destination table: table_dst', 'RDBMS_Dataset.insert;N/A%20in%20RDBMS;%5B3%2C%20%27source_new%27%2C%20datetime.datetime%282014%2C%201%2C%201%2C%200%2C%200%29%5D;Destination table: table_dst', 'RDBMS_Dataset.update;1;;Name : dest =>source;Destination table: table_dst', 'RDBMS_Dataset.update;0;;Name : dest =>source;Destination table: table_dst']
+        self.assertEqual(_d_dest._log, _log_cmp, "Logs differ")
         
+        print("result:\n" + str(_d_dest.data_table))
         
+        self.assertEqual(_d_dest.data_table,  [[1, 'source', datetime.datetime(2001, 1, 1, 0, 0)], [2, 'source', datetime.datetime(2001, 1, 2, 0, 0)], [3, 'source_new', datetime.datetime(2014, 1, 1, 0, 0)]], "Results differ")
         
+        _d_source.dal.close()
+        _d_dest.dal.close()
 
 
 if __name__ == "__main__":
