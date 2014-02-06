020adceaa8d9cafd60c8eeedd77b08eb1529f2da
Nicklas Börjesson <nicklas@optimalbpm.se>
2014-02-06 22:03:02 +0100

    Add quote string

--- qal/tools/tests/test_diff.py
+++ qal/tools/tests/test_diff.py
@@ -19,9 +19,9 @@ class Diff_test(unittest.TestCase):
 
     def test_diff(self):
         """Test comparison of two data sets, fetched from .csv files."""
-        _ff_source = Flatfile_Dataset(_filename = Test_Resource_Dir + "/csv_source.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL")
+        _ff_source = Flatfile_Dataset(_filename = Test_Resource_Dir + "/csv_source.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL", _quotechar='"')
         _dataset_source = _ff_source.load()
-        _ff_dest = Flatfile_Dataset(_filename = Test_Resource_Dir + "/csv_dest_orig.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL")
+        _ff_dest = Flatfile_Dataset(_filename = Test_Resource_Dir + "/csv_dest_orig.csv", _has_header = True, _delimiter = ";", _csv_dialect = "excel-tab", _quoting = "MINIMAL", _quotechar='"')
         _dataset_dest = _ff_dest.load()
         #print(str(_dataset_dest))
         _missing_left, _missing_right, _difference, _sorted = compare(_dataset_source, _dataset_dest, [0], True)
