73956296647976face4f495bab58a5f14a1cd735
Nicklas Börjesson <nicklas@optimalbpm.se>
2014-10-22 21:44:34 +0200

    Add support for writing .xls files

--- qal/dataset/tests/test_spreadsheet.py
+++ qal/dataset/tests/test_spreadsheet.py
@@ -17,19 +17,8 @@ import os
 Test_Script_Dir = os.path.dirname(__file__)
 Test_Resource_Dir = os.path.join(Test_Script_Dir, 'resources')
 
-def load_xml(_filename):
-    return etree.parse(_filename)
 
-class Test(unittest.TestCase):
-
-
-    def test_1_Load_Save_xls(self, _has_header = None, _resource = None):
-        _resources_node = load_xml(os.path.join(Test_Resource_Dir, "resources.xml")).find("resources")
-        _resources = Resources(_resources_node = _resources_node)
-        _da = SpreadsheetDataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22A1}"))
-        _da.load()
-        self.assertEqual(_da.data_table,
-            [
+_test_data= [
             [7369.0, 'SMITH', 'CLERK', 7902.0, '1980-12-17 00:00:00', 800.0, '', 20.0],
             [7499.0, 'ALLEN', 'SALE;SMAN', 7698.0, '1981-02-20 00:00:00', 1600.0, 300.0, 30.0],
             [7521.0, 'WARD', 'SALESMAN', 7698.0, '1981-02-22 00:00:00', 1250.0, 500.0, 30.0],
@@ -44,21 +33,25 @@ class Test(unittest.TestCase):
             [7902.0, 'FORD', 'ANALYST', 7566.0, '1981-12-03 00:00:00', 3000.0, '', 20.0],
             [7934.0, 'MILLER', 'CLERK', 7782.0, '1982-01-23 00:00:00', 1300.0, '', 10.0]
             ]
-        , "test_1_Load_Save: Loaded data doesn't match")
+
+def load_xml(_filename):
+    return etree.parse(_filename)
+
+class Test(unittest.TestCase):
 
 
+    def test_1_Load_Save_xls(self, _has_header = None, _resource = None):
+        _resources_node = load_xml(os.path.join(Test_Resource_Dir, "resources.xml")).find("resources")
+        _resources = Resources(_resources_node = _resources_node)
+        _da = SpreadsheetDataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22A1}"))
+        _da.load()
+        self.assertEqual(_da.data_table, _test_data, "test_1_Load_Save: Loaded data doesn't match")
 
-        _da.save(_save_as = Test_Resource_Dir + "/excel_out.xls")
-        pretty_list(_da.data_table)
+        _da.save(_save_as=Test_Resource_Dir + "/excel_out.xls")
 
-        # _f_a = open(Test_Resource_Dir + "/excel_out.csv", "r")
-        # _f_b = open(Test_Resource_Dir + "/excel_cmp.csv", "r")
-        # _a = _f_a.read()
-        # _b = _f_b.read()
-        # _f_a.close()
-        # _f_b.close()
-        # self.assertEqual(_a, _b, "test_1_Load_Save: Files are not equal")
-        #
+        _da.data_table = []
+        _da.load()
+        self.assertEqual(_da.data_table, _test_data, "test_1_Load_Save loading back: Loaded data doesn't match")
 
 
 if __name__ == "__main__":
