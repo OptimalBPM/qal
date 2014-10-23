64d6dc8525a12497f367b0142135ed8d6a47c2e3
Nicklas Börjesson <nicklas@optimalbpm.se>
2014-10-23 13:22:03 +0200

    Add support for writing .xlsx-files using openpyxl
    Reading .xlsx files is done using xlrd, since openpyxl doesn't handle column dimensions the preferred way yet

--- qal/dataset/spreadsheet.py
+++ qal/dataset/spreadsheet.py
@@ -8,16 +8,17 @@ from qal.common.strings import make_path_absolute, bool_to_binary_int, string_to
 from qal.dataset.custom import CustomDataset
 from qal.tools.discover import import_error_to_help
 
+
 def none_to_zero(_value):
     if _value is None or _value is False:
         return 0
     else:
         return _value
 
+
 class SpreadsheetDataset(CustomDataset):
- 
     """The Spreadsheet dataset can read data from a spreadsheet, currently only Excel files, and store that data in its."""
-    
+
     has_header = None
     """True if data begins with a header row containing field names."""
     filename = None
@@ -30,12 +31,13 @@ class SpreadsheetDataset(CustomDataset):
     """The leftmost column where data is held"""
     y_offset = None
     """The topmost row where data is held, *including the header row*."""
-    
+
     def read_resource_settings(self, _resource):
         self._base_path = _resource.base_path
 
         if _resource.type.upper() != 'SPREADSHEET':
-            raise Exception("SpreadsheetDataset.read_resource_settings.parse_resource error: Wrong resource type: " + _resource.type)
+            raise Exception(
+                "SpreadsheetDataset.read_resource_settings.parse_resource error: Wrong resource type: " + _resource.type)
         self.filename = _resource.data.get("filename")
         self.delimiter = _resource.data.get("delimiter")
         if _resource.data.get("has_header"):
@@ -50,7 +52,7 @@ class SpreadsheetDataset(CustomDataset):
         else:
             self.x_offset = 0
         if _resource.data.get("y_offset") is not None:
-            self.y_offset = int( _resource.data.get("y_offset"))
+            self.y_offset = int(_resource.data.get("y_offset"))
         else:
             self.y_offset = 0
 
@@ -63,25 +65,26 @@ class SpreadsheetDataset(CustomDataset):
         _resource.data["sheet_name"] = self.sheet_name
         _resource.data["x_offset"] = self.x_offset
         _resource.data["y_offset"] = self.y_offset
-    
-    def __init__(self, _filename = None, _has_header = None, _resource = None, _sheet_name = None, _x_offset = None, _y_offset = None):
+
+    def __init__(self, _filename=None, _has_header=None, _resource=None, _sheet_name=None, _x_offset=None,
+                 _y_offset=None):
         '''
         Constructor
         '''
-        super(SpreadsheetDataset, self ).__init__()
+        super(SpreadsheetDataset, self).__init__()
         if _resource != None:
-            self.read_resource_settings(_resource)        
+            self.read_resource_settings(_resource)
         else:
-  
-            if _filename != None: 
+
+            if _filename != None:
                 self.filename = _filename
             else:
-                self.filename = None      
-            if _has_header != None: 
+                self.filename = None
+            if _has_header != None:
                 self.has_header = _has_header
             else:
                 self.has_header = None
-            if _sheet_name != None: 
+            if _sheet_name != None:
                 self.sheet_name = _sheet_name
             else:
                 self.sheet_name = None
@@ -90,24 +93,24 @@ class SpreadsheetDataset(CustomDataset):
                 self.x_offset = _x_offset
             else:
                 self.x_offset = None
-            if _y_offset != None: 
+            if _y_offset != None:
                 self.y_offset = _y_offset
             else:
                 self.y_offset = None
-                  
+
     def load_xls(self, _filename, _sheet_name, _x_offset, _y_offset, _header_offset):
         print("SpreadsheetDataset.load_xls: using xlrd")
 
         try:
-            from xlrd import open_workbook
+            import xlrd
         except ImportError as _err:
             raise Exception(import_error_to_help(_module="xlrd", _err_obj=_err, _pip_package="xlrd",
                                                  _apt_package=None, _win_package=None))
         try:
-            _workbook = open_workbook(_filename)
+            _workbook = xlrd.open_workbook(_filename)
             _sheet = _workbook.sheet_by_name(_sheet_name)
 
-            if _header_offset >0:
+            if _header_offset > 0:
                 _field_names = _sheet.row_values(rowx=_y_offset, start_colx=_x_offset, end_colx=_sheet.ncols)
             else:
                 _field_names = list("Column_" + str(x) for x in range(_y_offset, _sheet.ncols))
@@ -124,11 +127,44 @@ class SpreadsheetDataset(CustomDataset):
 
     def load_xlsx(self, _filename, _sheet_name, _x_offset, _y_offset, _header_offset):
         print("SpreadsheetDataset.load_xlsx: using openpyxl")
+        try:
+            from openpyxl import load_workbook
+        except ImportError as _err:
+            raise Exception(import_error_to_help(_module="openpyxl",
+                                                 _err_obj=_err,
+                                                 _pip_package="openpyxl",
+                                                 _apt_package=None,
+                                                 _win_package=None))
+        try:
+            _workbook = load_workbook(_filename)
+            _sheet = _workbook.active
+            _number_of_columns = _sheet.get_highest_column()
+            _number_of_rows = _sheet.get_highest_row()
+            _data_table = []
+            for _curr_row in _sheet.iter_rows():
+                _data_table.append([_cell.value for _cell in _curr_row])
+
+            if _header_offset > 0:
+                _field_names = [_sheet.cell(column=_x, row=1 + _y_offset).value for _x in
+                                range(1 + _x_offset, _number_of_columns)]
+            else:
+                _field_names = list("Column_" + str(x) for x in range(1 + _x_offset, _number_of_columns))
+
+            for _curr_y in range(1 + _y_offset + _header_offset, _number_of_rows):
+                _data_table.append(
+                    [_sheet.cell(column=_x, row=_curr_y).value for _x in range(1 + _x_offset, _number_of_columns)])
+
+            return _data_table, _field_names
+
+        except IOError as e:
+            raise Exception("SpreadsheetDataset.load: Error reading file:" + str(e))
+
 
     def load(self):
         """Load data. """
 
         import os.path
+
         _extension = os.path.splitext(self.filename)[1]
 
         _x_offset = none_to_zero(self.x_offset)
@@ -141,56 +177,93 @@ class SpreadsheetDataset(CustomDataset):
 
         _filename = make_path_absolute(self.filename, self._base_path)
 
-        if _extension.lower() in [".xls"]:
+        if _extension.lower() in [".xls", ".xlsx"]:
             self.data_table, self.field_names = self.load_xls(_filename=_filename, _sheet_name=self.sheet_name,
-                                           _x_offset=_x_offset, _y_offset=_y_offset,
-                                           _header_offset = _header_offset)
-            
-            
+                                                              _x_offset=_x_offset, _y_offset=_y_offset,
+                                                              _header_offset=_header_offset)
+        # elif _extension.lower() in [".xlsx"]:
+        # self.data_table, self.field_names = self.load_xlsx(_filename=_filename, _sheet_name=self.sheet_name,
+        #                                    _x_offset=_x_offset, _y_offset=_y_offset,
+        #                                    _header_offset = _header_offset)
+
+
         elif _extension.lower() == ".odt":
             # from ezodf import newdoc, Paragraph, Heading, Sheet
             raise Exception("SpreadsheetDataset.load: Open Document Spreadsheet not implemented yet.")
 
         else:
-            raise Exception("SpreadsheetDataset.load: Unsupported file type \"" + _extension +"\"")
+            raise Exception("SpreadsheetDataset.load: Unsupported file type \"" + _extension + "\"")
+
     def save_xlsx(self, _filename, _data_table, _number_of_rows, _number_of_columns,
-                 _x_offset, _y_offset, _header_offset, _field_names):
-        pass
+                  _x_offset, _y_offset, _header_offset, _field_names, _sheet_name):
+        print("SpreadsheetDataset.save_xlsx: using openpyxl")
+
+        try:
+            import openpyxl
+        except ImportError as _err:
+            raise Exception(import_error_to_help(_module="openpyxl",
+                                                 _err_obj=_err,
+                                                 _pip_package="openpyxl",
+                                                 _apt_package=None,
+                                                 _win_package=None))
+
+        try:
+
+            _workbook = openpyxl.Workbook()
+            _sheet = _workbook.create_sheet(title = _sheet_name)
+
+            if _header_offset > 0:
+                for _curr_col_idx in range(1, _number_of_columns):
+
+                    _sheet.cell(row=1 + self.y_offset, column=_curr_col_idx + _x_offset).value = _field_names[_curr_col_idx]
+
+            for _curr_row_idx in range(1 + self.y_offset + _header_offset, _number_of_rows - 1):
+                for _curr_col_idx in range(1, _number_of_columns):
+                    _sheet.cell(row=_curr_row_idx, column=_curr_col_idx + _x_offset).value = \
+                        _data_table[_curr_row_idx][_curr_col_idx]
+
+            _workbook.save(filename=_filename)
+        except IOError as e:
+            raise Exception("SpreadsheetDataset.save_xlsx: Error saving file:" + str(e))
 
     def save_xls(self, _filename, _data_table, _number_of_rows, _number_of_columns,
-                 _x_offset, _y_offset, _header_offset, _field_names):
+                 _x_offset, _y_offset, _header_offset, _field_names, _sheet_name):
         print("SpreadsheetDataset.save_xls: using xlwt")
 
         try:
-            from xlwt import Workbook
+            import xlwt
         except ImportError as _err:
             raise Exception(import_error_to_help(_module="xlwt",
                                                  _err_obj=_err,
                                                  _pip_package="xlwt",
                                                  _apt_package=None,
                                                  _win_package=None))
+        try:
 
-        _workbook = Workbook()
-        _sheet = _workbook.add_sheet(sheetname="Sheet1", cell_overwrite_ok=True)
+            _workbook = xlwt.Workbook()
+            _sheet = _workbook.add_sheet(sheetname=_sheet_name, cell_overwrite_ok=True)
+            # Write headers
+            if _header_offset > 0:
+                for _curr_col_idx in range(0, _number_of_columns):
+                    _sheet.write(self.y_offset, _curr_col_idx + _x_offset, label=_field_names[_curr_col_idx])
+            # Write data
+            for _curr_row_idx in range(self.y_offset + _header_offset, _number_of_rows + self.y_offset + _header_offset - 1):
+                for _curr_col_idx in range(0, _number_of_columns):
+                    _sheet.write(_curr_row_idx + _y_offset + _header_offset, _curr_col_idx + _x_offset,
+                                 label=_data_table[_curr_row_idx][_curr_col_idx])
 
-        if _header_offset > 0:
-            for _curr_col_idx in range(0, _number_of_columns):
-                _sheet.write(self.y_offset, _curr_col_idx + _x_offset, label=_field_names[_curr_col_idx])
-
-        for _curr_row_idx in range(0, _number_of_rows + self.y_offset + _header_offset -1):
-            for _curr_col_idx in range(0, _number_of_columns):
-                _sheet.write(_curr_row_idx + _y_offset + _header_offset, _curr_col_idx + _x_offset,
-                label=_data_table[_curr_row_idx][_curr_col_idx])
-
-        _workbook.save(filename=_filename)
+            _workbook.save(filename=_filename)
+        except IOError as e:
+            raise Exception("SpreadsheetDataset.save_xls: Error saving file:" + str(e))
 
     def save(self, _save_as=None):
         """Save data"""
         _number_of_rows = len(self.data_table)
         if _number_of_rows > 0:
             _number_of_columns = len(self.data_table[0])
-        print("SpreadsheetDataset.save: Filename='" + str(self.filename) + "', Delimiter='"+str(self.delimiter)+"'")
+        print("SpreadsheetDataset.save: Filename='" + str(self.filename) + "', Delimiter='" + str(self.delimiter) + "'")
         import os.path
+
         _extension = os.path.splitext(self.filename)[1]
         if _save_as:
             _filename = _save_as
@@ -209,14 +282,21 @@ class SpreadsheetDataset(CustomDataset):
         if _extension.lower() == ".xls":
             self.save_xls(_filename, _data_table=self.data_table, _number_of_rows=_number_of_rows,
                           _number_of_columns=_number_of_columns, _x_offset=self.x_offset,
-                          _y_offset=self.y_offset, _header_offset=_header_offset, _field_names=self.field_names)
+                          _y_offset=self.y_offset, _header_offset=_header_offset, _field_names=self.field_names,
+                          _sheet_name=self.sheet_name)
 
         elif _extension.lower() == ".xlsx":
             self.save_xlsx(_filename, _data_table=self.data_table, _number_of_rows=_number_of_rows,
-                          _number_of_columns=_number_of_columns, _x_offset=self.x_offset,
-                          _y_offset=self.y_offset, _header_offset=_header_offset, _field_names=self.field_names)
+                           _number_of_columns=_number_of_columns, _x_offset=self.x_offset,
+                           _y_offset=self.y_offset, _header_offset=_header_offset, _field_names=self.field_names,
+                           _sheet_name=self.sheet_name)
 
+        elif _extension.lower() == ".odt":
+            # from ezodf import newdoc, Paragraph, Heading, Sheet
+            raise Exception("SpreadsheetDataset.load: Open Document Spreadsheet not implemented yet.")
 
+        else:
+            raise Exception("SpreadsheetDataset.load: Unsupported file type \"" + _extension + "\"")
         
 
     
--- qal/dataset/tests/test_spreadsheet.py
+++ qal/dataset/tests/test_spreadsheet.py
@@ -17,8 +17,10 @@ import os
 Test_Script_Dir = os.path.dirname(__file__)
 Test_Resource_Dir = os.path.join(Test_Script_Dir, 'resources')
 
+# There is a *minor* difference between the two input files, se CLERKX in the xlsx-variant.
+# This to be certain that the different files are being read.
 
-_test_data= [
+_test_data_xls= [
             [7369.0, 'SMITH', 'CLERK', 7902.0, '1980-12-17 00:00:00', 800.0, '', 20.0],
             [7499.0, 'ALLEN', 'SALE;SMAN', 7698.0, '1981-02-20 00:00:00', 1600.0, 300.0, 30.0],
             [7521.0, 'WARD', 'SALESMAN', 7698.0, '1981-02-22 00:00:00', 1250.0, 500.0, 30.0],
@@ -34,25 +36,55 @@ _test_data= [
             [7934.0, 'MILLER', 'CLERK', 7782.0, '1982-01-23 00:00:00', 1300.0, '', 10.0]
             ]
 
+
+_test_data_xlsx= [
+            [7369.0, 'SMITH', 'CLERKX', 7902.0, '1980-12-17 00:00:00', 800.0, '', 20.0],
+            [7499.0, 'ALLEN', 'SALE;SMAN', 7698.0, '1981-02-20 00:00:00', 1600.0, 300.0, 30.0],
+            [7521.0, 'WARD', 'SALESMAN', 7698.0, '1981-02-22 00:00:00', 1250.0, 500.0, 30.0],
+            [7566.0, 'JONES', 'MANAGER', 7839.0, '1981-04-02 00:00:00', 2975.0, '', 20.0],
+            [7654.0, 'MARTIN', 'SALESMAN', 7698.0, '1981-09-28 00:00:00', 1250.0, 1400.0, 30.0],
+            [7698.0, 'BLAKE', 'MANAGER', 7839.0, '1981-05-01 00:00:00', 2850.0, '', 30.0],
+            [7782.0, 'CLARK', 'MANAGER', 7839.0, '1981-06-09 00:00:00', 2450.0, '', 10.0],
+            [7788.0, 'SCOTT', 'ANALYST', 7566.0, '1982-12-09 00:00:00', 3000.0, '', 20.0],
+            [7839.0, 'KING', 'PRESIDENT', '', '1981-11-17 00:00:00', 5000.0, '', 10.0],
+            [7876.0, 'ADAMS', 'CLERK', 7788.0, '1983-01-12 00:00:00', '1100,5', '', 20.0],
+            [7900.0, 'JAMES', 'CLERK', 7698.0, '1981-12-03 00:00:00', 950.0, '', 30.0],
+            [7902.0, 'FORD', 'ANALYST', 7566.0, '1981-12-03 00:00:00', 3000.0, '', 20.0],
+            [7934.0, 'MILLER', 'CLERK', 7782.0, '1982-01-23 00:00:00', 1300.0, '', 10.0]
+            ]
+
+
 def load_xml(_filename):
     return etree.parse(_filename)
 
 class Test(unittest.TestCase):
+    """Reads, writes, and reads again .xls and .xlsx-files while comparing with stored matrixes off data"""
+    def _test_1_Load_Save_xls(self, _has_header = None, _resource = None):
 
-
-    def test_1_Load_Save_xls(self, _has_header = None, _resource = None):
         _resources_node = load_xml(os.path.join(Test_Resource_Dir, "resources.xml")).find("resources")
         _resources = Resources(_resources_node = _resources_node)
         _da = SpreadsheetDataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22A1}"))
         _da.load()
-        self.assertEqual(_da.data_table, _test_data, "test_1_Load_Save: Loaded data doesn't match")
+        self.assertEqual(_da.data_table, _test_data_xls, "test_1_Load_Save: Loaded data doesn't match")
 
         _da.save(_save_as=Test_Resource_Dir + "/excel_out.xls")
 
         _da.data_table = []
         _da.load()
-        self.assertEqual(_da.data_table, _test_data, "test_1_Load_Save loading back: Loaded data doesn't match")
+        self.assertEqual(_da.data_table, _test_data_xls, "test_1_Load_Save loading back: Loaded data doesn't match")
 
+    def test_2_Load_Save_xlsx(self, _has_header = None, _resource = None):
+        _resources_node = load_xml(os.path.join(Test_Resource_Dir, "resources.xml")).find("resources")
+        _resources = Resources(_resources_node = _resources_node)
+        _da = SpreadsheetDataset(_resource= _resources.get_resource("{86470370-FF78-48A4-9759-A3BAE4EE22A2}"))
+        _da.load()
+        self.assertEqual(_da.data_table, _test_data_xlsx, "test_2_Load_Save: Loaded data doesn't match")
+
+        _da.save(_save_as=Test_Resource_Dir + "/excel_out.xlsx")
+
+        _da.data_table = []
+        _da.load()
+        self.assertEqual(_da.data_table, _test_data_xlsx, "test_2_Load_Save loading back: Loaded data doesn't match")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
