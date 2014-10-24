28bcfb1cb895134b933d42641ac760e14a165ce9
Nicklas Börjesson <nicklas@optimalbpm.se>
2014-10-24 22:38:05 +0200

    Clean up more

--- qal/common/transform.py
+++ qal/common/transform.py
@@ -282,8 +282,7 @@ class ReplaceRegex(CustomTransformation):
             _value = ""
         if (self.pattern is None):
             raise Exception("ReplaceRegex.transform: pattern has to have a value.")
-        else:
-            if self.compiled_regex is None or self.compiled_regex.pattern != self.pattern:
+        elif self.compiled_regex is None or self.compiled_regex.pattern != self.pattern:
                 self.compiled_regex = re.compile(self.pattern)
 
         if self.compiled_regex.search(_value):
