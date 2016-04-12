dbb2a6c818cbee367d24479324ff146f0a8deff7
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-04-12 19:36:54 +0200

    Release 0.6

--- qal/__init__.py
+++ qal/__init__.py
@@ -10,8 +10,8 @@
 
 """
 # Version and release information used by Sphinx for documentation and setuptools for package generation. 
-__version__ = '0.5'
-__release__ = '0.5.1'
+__version__ = '0.6'
+__release__ = '0.6'
 __copyright__ = '2010-2015, Nicklas Boerjesson'
 
 
