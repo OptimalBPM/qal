4d7a31c0d68042b4110e1fa3e733711e0fdd473e
Nicklas Börjesson <nicklas@optimalbpm.se>
2016-05-20 11:52:07 +0200

    Add better error handling

--- qal/dataset/factory.py
+++ qal/dataset/factory.py
@@ -12,6 +12,10 @@ from qal.dataset.spreadsheet import SpreadsheetDataset
 
 def dataset_from_resource(_resource):
     """Create a qal dataset from a resource"""
+    if _resource.uuid is None:
+        raise Exception("CustomDataset.check_resource: A resource must have an uuid")
+    if _resource.type is None:
+        raise Exception("CustomDataset.check_resource: A resource must have a resource type, resource id: " + str(_resource.uuid))
     try:
         if _resource.type.upper() == "FLATFILE":
             _ds = FlatfileDataset(_resource=_resource)
@@ -25,10 +29,10 @@ def dataset_from_resource(_resource):
             _ds = FilesDataset(_resource=_resource)
         else:
             raise Exception("qal.dataset.factory.dataset_from_resource: Unsupported source resource type: " + str(
-                _resource.type.upper()))
+                _resource.type.upper()) + " for resource id: " + str(_resource.uuid))
     except Exception as e:
         raise Exception(
-            "qal.dataset.factory.dataset_from_resource: Failed loading resource for " + _resource.type.upper() + ".\n" +
+            "qal.dataset.factory.dataset_from_resource: Failed loading resource for " + str(_resource.type) + ".\n" +
             "Resource: " + str(_resource.caption) + "(" + str(_resource.uuid) + ")\n" +
             "Error: \n" + str(e))
 
